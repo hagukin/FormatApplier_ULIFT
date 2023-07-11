f5ffe96103d56e5b7075ac86d3e8731351c1065a
ulift <ulift.ca@gmail.com>
2023-07-11 17:44:56 +0900

    feature: fstring 내부 중괄호 및 중괄호 내부 코드에 대해 색상 태그 자동화 기능 추가

--- formatApplier.py
+++ formatApplier.py
@@ -61,6 +61,8 @@ class FormatConfig():
                 "f_string_tag" : "blue",
                 "string" : "orange",
                 "comment" : "green",
+                "f_string_bracket" : "pink",
+                "f_string_bracket_inner" : "light-gray",
                 "bracket" : "pink",
                 "number" : "mint",
                 "default" : "light-gray",
@@ -160,16 +162,17 @@ class FormatApplier():
         raise NotImplementedError()
     
     def return_overlapped(self, line: str) -> list[bool]:
-        # 어떠한 태그도 적용되지 않는 구간을 리스트로 반환
-        overlapped = [False] * len(line)
+        # 각 구간 별 어떤 태그를 적용중인지를 리스트로 반환.
+        overlapped = [""] * len(line)
         new_line = line
         tag_opened = -1
         for i in range(len(new_line)-1):
-            if tag_opened == -1 and new_line[i] == '<':
+            if tag_opened == -1 and new_line[i] == "<":
                 tag_opened = i
-            if new_line[i:i+2] == '</' and tag_opened != -1:
+            if new_line[i:i+2] == "</" and tag_opened != -1:
+                tag_end = new_line.find(">",i)
                 for j in range(tag_opened, i):
-                    overlapped[j] = True
+                    overlapped[j] = new_line[i+2:tag_end] # 태그명으로 저장
         return overlapped
     
     def find_all_numbers(self, line: str) -> list[any]:
@@ -259,6 +262,46 @@ class PythonFormatApplier(FormatApplier):
             found = new_line.find(search_for, int(found) + len("f") + (len(tag_name)*2) + 5) # 태그 추가로 인해 늘어난 길이만큼 더한다 = tag_name*2 + 5 (<x> </x>) 
         return new_line
     
+    def apply_color_format_f_string_bracket(self, line: str) -> str:
+        # f string 내부의 중괄호 및 중괄호 내부의 코드에 대해 태그를 추가한다
+        new_line = line
+        str_tag = self.config["colors"]["python"]["string"] # 문자열
+        br_tag = self.config["colors"]["python"]["f_string_bracket"] # f string 내부의 괄호 색상
+        inner_tag = self.config["colors"]["python"]["f_string_bracket_inner"] # 괄호 내부의 코드 색상
+
+        br_begin = new_line.find("{")
+        br_end = new_line.find("}")
+
+        while br_begin != -1 and br_end != 1:
+            overlapped = self.return_overlapped(new_line)
+            if overlapped[br_begin] != str_tag or overlapped[br_end] != str_tag: # NOTE: 좋은 방식은 아님 (string이 아닌 곳에서도 string과 동일한 태그를 사용할 수 있음)
+                br_begin = new_line.find("{", br_end+1)
+                br_end = new_line.find("}", br_end+1)
+                continue
+            
+            new_line = self.apply_tag_tail(new_line, br_begin, str_tag)
+            br_end += 3+len(str_tag) # </x>
+            br_begin += 3+len(str_tag)
+
+            new_line = self.apply_tag(new_line, br_begin, br_begin+len("{"), br_tag)
+            br_end += (2+len(br_tag))*2 + 1 # <x></x>
+            br_begin += (2+len(br_tag))*2 + 1
+
+            new_line = self.apply_tag(new_line, br_begin + 1, br_end, inner_tag)
+            br_end += (2+len(inner_tag))*2 + 1
+
+            new_line = self.apply_tag(new_line, br_end, br_end+len("}"), br_tag)
+            br_end += (2+len(br_tag))*2 + 1 # <x></x>
+
+            new_line = self.apply_tag_head(new_line, br_end+1, str_tag)
+            br_end += 2+len(str_tag) # <x>
+
+            br_begin = new_line.find("{", br_end)
+            if br_begin == -1:
+                break
+            br_end = new_line.find("}", br_begin+1)
+        return new_line
+    
     def apply_color_format_bracket(self, line: str) -> str:
         new_line = line
         br_begin = new_line.find("{")
@@ -322,7 +365,7 @@ class PythonFormatApplier(FormatApplier):
                 break
             if found > 0 and new_line[found-1].isalpha():
                 continue # num1 과 같이 변수명 뒤의 숫자 허용
-            if overlapped[found] == True:
+            if overlapped[found]:
                 continue # 태그 중복 방지
 
             tag_name = self.config["colors"]["python"]["number"]
@@ -336,6 +379,7 @@ class PythonFormatApplier(FormatApplier):
         new_line = self.apply_color_format_fstring(new_line)
         new_line = self.apply_color_format_string(new_line)
         new_line = self.apply_color_format_bracket(new_line)
+        new_line = self.apply_color_format_f_string_bracket(new_line)
         new_line = self.apply_color_format_comment(new_line)
         return new_line
     
@@ -394,6 +438,7 @@ class PythonFormatApplier(FormatApplier):
 def start() -> int:
     fa = PythonFormatApplier("python")
     fa.run()
+    input("종료하시려면 아무 키나 누르십시오.")
     return 0
 
 if __name__ == "__main__":
